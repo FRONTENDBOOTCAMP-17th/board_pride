778be51bb5b4e30d89718766c7864adb1248f266
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-03-19 17:17:12 +0900

    fix: api 개선

--- pages/comments/apiHandler.js
+++ pages/comments/apiHandler.js
@@ -1,16 +1,16 @@
 export async function request(url, options){
     // 로컬스토리지 토큰 저장 하나 만들기
     try{
-        const token = localStorage.getItem("accessToken");
+        const token = localStorage.getItem("token");
         const res = await fetch(url,{
+            ...options,
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${token}`,
             },
-            ...options,
         });
         
-        const data = await res.JSON();
+        const data = await res.json();
         
         if(!res.ok){
             console.error("잘못된 접근입니다.");
